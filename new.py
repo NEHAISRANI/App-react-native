9f403d9f5a58fc26acfad8ba9d4881b19cf17c65
NEHAISRANI <47298416+NEHAISRANI@users.noreply.github.com>
2021-08-17 17:05:21 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,17 @@
+# i=1
+# while i<=2:
+#     name=input("enter name")
+#     if name[0]>'a' and name[0]<='z':
+        
+#         name[0]=name[0].upper()
+#     else:
+#         name[0]=name[0]
+#     print(name)
+#     i=i+1
+
+i=145
+sum=0
+while i<=438:
+    sum=sum+i
+    i=i+1
+print(sum)
